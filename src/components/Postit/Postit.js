7d53235a1b37c55a125beb1599ed3919dd598813
federico1995-tech <federicotiralongo95@gmail.com>
2021-01-15 20:07:26 +0100

    componente postit

--- src/components/Postit/Postit.js
+++ src/components/Postit/Postit.js
@@ -1,26 +1,24 @@
 import "./Postit.css";
 
-const Postit = ({button}) => {
+const Postit = () => { 
+    const handleClick = () => {
+        console.log('hola')
+    }
+
+
      return (
        
-         <div className="postit__container">
-             <div className="postitTitle__container">
-             <div className="postit__title">Los mejores productos usados</div>
-         </div>
-
-
-                <div className="postitDescription__container">
-                <div className="postit__description">Compra productos usados y ahorra!!</div>
-                
-               
-            
-
-                <div className="ButtonContainer">
-                <div className="MyButton" onClick={button}>Compra Ya!</div>
-                </div>
-
-            </div>
-            </div>
+        <div className="postit__container">
+            <div className="postitTitle__container">
+            <div className="postit__title">Los mejores productos usados</div>
+        </div>
+        <div className="postitDescription__container">
+            <div className="postit__description">Compra productos usados y ahorra!!</div>
+            <div className="ButtonContainer">
+            <div className="MyButton" onClick={handleClick}>Compra Ya!</div>
+        </div>
+        </div>
+        </div>
        
      );
      }
