c281515af045ffef513fc7cf2487a60e0e22a067
federico1995-tech <federicotiralongo95@gmail.com>
2021-01-20 19:36:27 +0100

    añadir elementos

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Home from './pages/home/home';
 
 
 
+
 function App() {
   
   return (
     <div>
-      <Home />
+      <Home/>
       
     </div>
 
--- src/components/postit/postit.js
+++ src/components/postit/postit.js
@@ -1,4 +1,4 @@
-import "./Postit.css";
+import "./postit.css";
 
 
 
