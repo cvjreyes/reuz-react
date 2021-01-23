5b73d46c13e34ab42bde70023d6a0787a10ef08f
federico1995-tech <federicotiralongo95@gmail.com>
2021-01-23 14:36:21 +0100

    casi arreglo del header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,6 +3,8 @@ import logo from '../../assets/image10.svg';
 import desplegable from '../../assets/Line.svg';
 import vector from '../../assets/Vector.svg';
 import userimagen from '../../assets/Ellipse2.svg';
+import carousel from '../../assets/car.svg';
+import carousel2 from '../../assets/bici.svg';
 
 
 const Header = () => {
@@ -11,11 +13,14 @@ const Header = () => {
   } 
   return (
     <div className="header__container">
+       <span classeName="productos">Products</span>
       <img className="logito" alt="el logo" src={logo} onClick={handleClick}/>
-      <span classeName="productos">Products</span>
       <img className="flecha" alt="flechita" src={desplegable} onClick={handleClick}/>
       <img className="vector" alt="♥" src={vector}/>
       <img className="userimagen" alt="immagine" src={userimagen}/>
+      <img className="carousel" alt="car" src={carousel}/>
+      <img className="carousel2" alt="bicicleta" src={carousel2}/>
+
     </div>
   );
 };
