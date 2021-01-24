3c6dd17f5c326ac31d29758d66658b4e97ef9ba2
Jorge <cv.jreyes@gmail.com>
2021-01-24 21:33:03 +0100

    Barra de iconos organizada

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,7 +4,7 @@ import Icon from "../../components/icon/icon";
 import Footer from "../../components/footer/footer";
 import "./home.css";
 //import Buscador from "../../components/buscador/buscador";
-import { Input } from 'antd';
+//import { Input } from 'antd';
 //import { Carousel } from 'antd';
 //import 'antd/dist/antd.css';
 //import { image } from "./assets
@@ -13,19 +13,16 @@ import Header from "../../components/header/header"
 export const Home = () => {
   return (
     <div>
-        <Input placeholder="Search for anything!" className="buscador__container" />
+        {/* <Input placeholder="Search for anything!" className="buscador__container" /> */}
         <Header />
         <Postit 
         title="The best second hand items"
         description="Our forniture is made from selected and best quality materials that are suitable for your dream home"
         button="Compra ya!"/>
-        <Icon 
-            urlImage="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/How_to_use_icon.svg/1200px-How_to_use_icon.svg.png" 
-            title="Champions in variety" 
-            description="Thousands of products"
-            icon1="imagen iconica"/>
-            <Footer />
+        <Icon />
+        
         <ProductSelection />
+        <Footer />
     </div>  
   )
 };
