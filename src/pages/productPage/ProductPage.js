a2a7817f8e5505d45fdef4ebdbb4733459cd2909
raulstvz <estevezterrados@gmail.com>
2021-03-03 10:45:08 +0100

    Advances in product page features

--- src/pages/productPage/ProductPage.js
+++ src/pages/productPage/ProductPage.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import ContactIcon from '../../assets/message.svg';
 import { useParams } from 'react-router';
 import { MapContainer, TileLayer, Circle, useMap } from 'react-leaflet';
+import Modal from "../../components/modal/Modal"
+import SignupForm from "../../components/forms/SignupForm"
+import LoginForm from "../../components/forms/LoginForm"
 
 // Styles for the page and the map
 import './ProductPage.css'
@@ -9,9 +12,9 @@ import Leaflet from 'leaflet/dist/leaflet.css'
 
 const ProductPage = () => {
 
+    /* Product fetch */
     const params = useParams();
-    const productId = params.id
-
+    const productId = params.id;
     const [productInfo, setProductInfo] = useState();
 
     useEffect(() => {
@@ -28,17 +31,101 @@ const ProductPage = () => {
             });
     }, []);
 
-    console.log(productInfo)
-
     // Position after geocoding process
     const productPosition = [40.42, -3.70]
+
+
+    /* User recognition */
+    const [knownUser, setKnownUser] = useState(false);
+    const [loggedUser, setLoggedUser] = useState('User not logged in');
+
+
+    console.log(knownUser)
+
+    const checkUserLoggedIn = () => {
+        const userLoggedIn = localStorage.getItem('user');
+        if (userLoggedIn) {
+            setKnownUser(true);
+            const userId = JSON.parse(userLoggedIn).id;
+            fetch("http://localhost:5000/api/users/" + userId)
+                .then((response) => response.json())
+                .then((json) => setLoggedUser(json));
+            handleChatModal();
+        } else {
+            setKnownUser(false);
+            handleLoginModal();
+        }
+    };
+
+
+    /* Modal control */
+    const [imageModalVisibility, setImageModalVisibility] = useState(false);
+    const [chatModalVisibility, setChatModalVisibility] = useState(false);
+    const [signupVisibility, setSignupVisibility] = useState(false);
+    const [loginVisibility, setLoginVisibility] = useState(false);
+
+    const handleImageModal = () => {
+        setImageModalVisibility(!imageModalVisibility)
+    }
+
+    const handleChatModal = () => {
+        setChatModalVisibility(!chatModalVisibility)
+    }
+
+    const handleLoginModal = () => {
+        setLoginVisibility(!loginVisibility)
+    }
+
+    const handleSignupModal = () => {
+        setSignupVisibility(!signupVisibility)
+    }
+
+    const changeModal = () => {
+        handleLoginModal();
+        handleSignupModal();
+    }
+
 
     return (
         <>
             {productInfo &&
                 <div className="productPage__container">
                     <div className="productPage_images__container">
-                        <p>Here go the images</p>
+                        <div className="productPage_images_firstImage">
+                            <img
+                                src={productInfo.urlImage}
+                                alt="product display"
+                                onClick={handleImageModal}
+                            />
+                        </div>
+                        <div className="productPage_images_otherImages_up">
+                            <img
+                                src={productInfo.urlImage}
+                                alt="product display"
+                                onClick={handleImageModal}
+                            />
+                        </div>
+                        <div className="productPage_images_otherImages_down">
+                            <img
+                                src={productInfo.urlImage}
+                                alt="product display"
+                                onClick={handleImageModal}
+                            />
+                        </div>
+                        <div className="productPage_images_otherImages_up">
+                            <img
+                                src={productInfo.urlImage}
+                                alt="product display"
+                                onClick={handleImageModal}
+                            />
+                        </div>
+                        <div className="productPage_images_otherImages_down">
+                            <img
+                                src={productInfo.urlImage}
+                                alt="product display"
+                                onClick={handleImageModal}
+                            />
+                        </div>
                     </div>
                     <div className="productPage_info__container">
                         <div className="productPage_info_area">
@@ -55,7 +142,7 @@ const ProductPage = () => {
                                     Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one
                                     of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through
                                     the cites of the word in classical literature, discovered the undoubtable source.
-                        </p>
+                                </p>
                                 <h2 className="productPage_info_subtitle">Precio y condiciones</h2>
                                 <p className="productPage_info_price">{productInfo.price}€</p>
                             </div>
@@ -63,7 +150,8 @@ const ProductPage = () => {
                         <div className="productPage_contact_area">
                             <div className="productPage_contact_block">
                                 <h2>Start a conversation with the seller!</h2>
-                                <div className="productPage_contact_button">
+                                <div className="productPage_contact_button"
+                                    onClick={checkUserLoggedIn}>
                                     <img src={ContactIcon} />
                                     <span>Send a message</span>
                                 </div>
@@ -90,13 +178,48 @@ const ProductPage = () => {
                                 fillColor={'#F9F1E7'}
                                 fillOpacity={0.25} >
                             </Circle>
-                            {/* <DinamicBounds
-                    position={productPosition}
-                    distance={100}
-                    zoom={5} 
-                /> */}
+                            {/* 
+                            <DinamicBounds
+                                position={productPosition}
+                                distance={100}
+                                zoom={5} 
+                            /> 
+                            */}
                         </MapContainer>
                     </div>
+                    {/* Modals */}
+                    <Modal
+                        visibility={imageModalVisibility}
+                        setVisibility={setImageModalVisibility}
+                        content={
+                            <div className="productPage_images_modalImage">
+                                <img
+                                    src={productInfo.urlImage}
+                                    alt="product display"
+                                    onClick={handleImageModal}
+                                />
+                            </div>
+                        }
+                    />
+                    <Modal
+                        visibility={chatModalVisibility}
+                        setVisibility={setChatModalVisibility}
+                        content="Here goes the chat"
+                    />
+                    <Modal
+                        visibility={signupVisibility}
+                        setVisibility={setSignupVisibility}
+                        content={
+                            <SignupForm changeModal={changeModal} action={checkUserLoggedIn} />
+                        }
+                    />
+                    <Modal
+                        visibility={loginVisibility}
+                        setVisibility={setLoginVisibility}
+                        content={
+                            <LoginForm changeModal={changeModal} action={checkUserLoggedIn} />
+                        }
+                    />
                 </div>
             }
         </>
