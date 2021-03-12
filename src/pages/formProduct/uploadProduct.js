7f821c77171c97e3b0db406ef66377e39982f4bf
Felipe <f.zende@gmail.com>
2021-03-12 19:24:44 +0100

    test

--- src/pages/formProduct/uploadProduct.js
+++ src/pages/formProduct/uploadProduct.js
@@ -1,19 +1,12 @@
-import React, { useState } from 'react';
-import ProductDetails from "../../components/forms/productDetails"
-import ProductImages from "../../components/forms/productImages"
+import React, { useState} from 'react';
+import ProductDetails from "../../components/forms/productDetails";
+import ProductImages from "../../components/forms/productImages";
 import "./uploadProduct.css";
 import ListBody from 'antd/lib/transfer/ListBody';
+import PhotoLoader from "../../components/photoLoader/photoLoader";
 
 const UpLoadProduct = () => {
-
     /* Controls the form to be rendered */
-    const [visibleForm, setVisibleForm] = useState("first")
-    const handlevisibleForm = () => {
-
-
-        setVisibleForm("second")
-    }
-
     /* Controls the fetch heading towards MongoDB at API endpoint */
     //formData : combo for the inputs
     const [formData, setFormData] = useState({
@@ -21,20 +14,15 @@ const UpLoadProduct = () => {
         price: undefined,
         discount: undefined,
         address: undefined,
-        description: undefined
     });
-
     //Body: conforms the key/values to be send to MongoDB
-    const body = {
+    const firstbody = {
         name: formData.name,
         price: formData.price,
         discount: formData.discount,
         address: formData.address,
-        description: formData.description
     };
-
-    console.log(body);
-
+    console.log(firstbody);
     //Executes the fetch function at the end of the form process
     const handleCreate = () => {
         const options = {
@@ -42,27 +30,85 @@ const UpLoadProduct = () => {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(),
+            firstbody: JSON.stringify(firstbody),
         };
         fetch('http://localhost:5000/api/products', options);
         console.log("Product created");
     }
-
-    return (
-        <div>
-            {visibleForm === "first" &&
-                <ProductDetails
-                    formData={formData}
-                    setFormData={setFormData}
-                    action={handlevisibleForm} />
+    const [visibleForm, setVisibleForm] = useState("first")
+    const handlevisibleForm = () => {
+        setVisibleForm("second")
+    }
+    const handleCreateNext = () => {
+        handleCreate()
+        handlevisibleForm()
+    }
+    /*//////////////////////////////////*/
+    const [descData, setDescData] = useState({
+        description: undefined
+    });
+    //Body: conforms the key/values to be send to MongoDB
+    const secondbody = {
+        description: descData.description
+    };
+    const updateDescription = () => {
+        const options = {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(secondbody),
+        };
+        fetch('http://localhost:5000/api/categories/products/${id}', options);
+        console.log("Product created");
+    }
+    const [photoArray, setPhotoArray] = useState([]);
+    //Body: conforms the key/values to be send to MongoDB
+    const photoLoad = {
+        photo: photoArray.photo
+    };
+    const photos = () => {
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(photoLoad)
+        };
+        fetch('http://localhost:5000/api/photos/${id}');
+        console.log("Photo uploaded");
+    };
+    return ( <
+        div > {
+            visibleForm === "first" &&
+            <
+            ProductDetails
+            formData = {
+                formData
             }
-            {visibleForm === "second" &&
-                <ProductImages
-                    formData={formData}
-                    setFormData={setFormData}
-                    action={handleCreate} />
+            setFormData = {
+                setFormData
             }
-        </div>
+            action = {
+                handleCreateNext
+            }
+            />
+        } {
+            visibleForm === "second" &&
+                <
+                ProductImages
+            photoArray = {
+                photoArray
+            }
+            setPhotoArray = {
+                setPhotoArray
+            }
+            secondAction = {
+                photos
+            }
+            />
+        } <
+        /div>
     )
 }
 export default UpLoadProduct;
